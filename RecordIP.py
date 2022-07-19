994ad83b2b0459983e22ddd274250f55820cd459
username <useremail@useremail.com>
2022-07-19 23:21:06 +0800

    1.0.3

--- RecordIP.py
+++ RecordIP.py
@@ -11,9 +11,9 @@ while True:
     newip = json.loads(r.text) #json解析爬取的文字
     if (newip["origin"] != ip):
         current_time = datetime.datetime.now().strftime("%m月%d日%H:%M")
-        with open('./log.txt', 'wt', encoding='utf-8') as f: #记录日志文件
-            f.write('在'+current_time+'左右更改了ip，现在的IP地址为'+newip["origin"])
+        with open('./log.txt', 'a+', encoding='utf-8') as f: #记录日志文件
+            f.write('在'+current_time+'左右更改了ip，现在的IP地址为'+newip["origin"]+"\n")
             f.close()
         print('在',current_time,'左右更改了ip，现在的IP地址为',newip["origin"])
-        ip = newip
+        ip = newip["origin"]
     time.sleep(600) #延迟10分钟
